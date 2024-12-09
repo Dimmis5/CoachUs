6144806b5c278926b8cd79852e9af40e1cd373a1
Dimmis5 <zhangdimeo@gmail.com>
2024-12-09 14:02:51 +0100

    j'ai récupé les fichiers

---
+++ connexion (1).php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "coachus";
+
+// Créer la connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("Connexion échouée : " . $conn->connect_error);
+}
+
+?>
