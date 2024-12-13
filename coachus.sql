f637d7be82a3b8c4dbda48e07aba95731830efb1
Oriane5 <orco63803@eleve.isep.fr>
2024-12-13 14:36:10 +0100

    php

--- coachus.sql
+++ coachus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 04 déc. 2024 à 11:25
+-- Généré le : dim. 08 déc. 2024 à 11:57
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -18,22 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `coachus`
+-- Base de données : `coachus_essai`
 --
-
--- --------------------------------------------------------
-
---
--- Structure de la table `administrateur`
---
-
-CREATE TABLE `administrateur` (
-  `id_administrateur` int(11) NOT NULL,
-  `login` varchar(100) NOT NULL,
-  `mot_de_passe` varchar(100) NOT NULL,
-  `nom` varchar(100) NOT NULL,
-  `prenom` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -43,7 +29,7 @@ CREATE TABLE `administrateur` (
 
 CREATE TABLE `coach` (
   `id_coach` int(11) NOT NULL,
-  `login` varchar(100) NOT NULL,
+  `identifiant` varchar(100) NOT NULL,
   `mot_de_passe` varchar(100) NOT NULL,
   `nom` varchar(100) NOT NULL,
   `prenom` varchar(100) NOT NULL,
@@ -64,32 +50,7 @@ CREATE TABLE `disponibilite` (
   `heure_debut` time NOT NULL,
   `heure_fin` time NOT NULL,
   `id_coach` int(11) NOT NULL,
-  `id_lieu` int(11) NOT NULL,
-  `id_reservation` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `faq_coach`
---
-
-CREATE TABLE `faq_coach` (
-  `id_question` int(11) NOT NULL,
-  `question` varchar(1000) NOT NULL,
-  `reponse` varchar(10000) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `faq_sportif`
---
-
-CREATE TABLE `faq_sportif` (
-  `id_question` int(11) NOT NULL,
-  `question` varchar(1000) NOT NULL,
-  `reponse` varchar(10000) DEFAULT NULL
+  `id_lieu` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -102,98 +63,12 @@ CREATE TABLE `lieu` (
   `id_lieu` int(11) NOT NULL,
   `nom` varchar(500) NOT NULL,
   `adresse` varchar(500) NOT NULL,
-  `nombre_places_disponibles` int(11) DEFAULT NULL,
-  `id_coach` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `messagerie1`
---
-
-CREATE TABLE `messagerie1` (
-  `id_message` int(11) NOT NULL,
-  `date` date NOT NULL,
-  `heure` time NOT NULL,
-  `contenu` varchar(10000) DEFAULT NULL,
-  `id_sportif` int(11) NOT NULL,
-  `id_coach` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `messagerie2`
---
-
-CREATE TABLE `messagerie2` (
-  `id_message` int(11) NOT NULL,
-  `date` date NOT NULL,
-  `heure` time NOT NULL,
-  `contenu` varchar(10000) DEFAULT NULL,
-  `id_sportif` int(11) NOT NULL,
-  `id_coach` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `reservation`
---
-
-CREATE TABLE `reservation` (
-  `id_reservation` int(11) NOT NULL,
-  `note` int(11) NOT NULL,
-  `commentaire` varchar(1000) NOT NULL,
-  `disponibilite` int(11) NOT NULL,
-  `id_sport` int(11) NOT NULL,
-  `id_sportif` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `sport`
---
-
-CREATE TABLE `sport` (
-  `id_sport` int(11) NOT NULL,
-  `nom` varchar(100) NOT NULL,
-  `id_lieu` int(11) NOT NULL,
-  `id_sportif` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `sportif`
---
-
-CREATE TABLE `sportif` (
-  `id_sportif` int(11) NOT NULL,
-  `login` varchar(100) NOT NULL,
-  `mot_de_passe` varchar(100) NOT NULL,
-  `nom` varchar(100) NOT NULL,
-  `prenom` varchar(100) NOT NULL,
-  `adresse` varchar(500) NOT NULL,
-  `adresse_mail` varchar(100) NOT NULL,
-  `distance_max` float NOT NULL,
-  `id_message_recue` int(11) NOT NULL,
-  `id_message_envoye` int(11) NOT NULL,
-  `id_sport` int(11) NOT NULL,
-  `id_reservation` int(11) NOT NULL
+  `nombre_places_disponibles` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Index pour les tables déchargées
 --
-
---
--- Index pour la table `administrateur`
---
-ALTER TABLE `administrateur`
-  ADD PRIMARY KEY (`id_administrateur`);
 
 --
 -- Index pour la table `coach`
@@ -205,77 +80,17 @@ ALTER TABLE `coach`
 -- Index pour la table `disponibilite`
 --
 ALTER TABLE `disponibilite`
-  ADD PRIMARY KEY (`id_disponibilite`),
-  ADD KEY `id_coach` (`id_coach`),
-  ADD KEY `id_lieu` (`id_lieu`),
-  ADD KEY `id_reservation` (`id_reservation`);
-
---
--- Index pour la table `faq_coach`
---
-ALTER TABLE `faq_coach`
-  ADD PRIMARY KEY (`id_question`);
-
---
--- Index pour la table `faq_sportif`
---
-ALTER TABLE `faq_sportif`
-  ADD PRIMARY KEY (`id_question`);
+  ADD PRIMARY KEY (`id_disponibilite`);
 
 --
 -- Index pour la table `lieu`
 --
 ALTER TABLE `lieu`
-  ADD PRIMARY KEY (`id_lieu`),
-  ADD KEY `FOREIGN KEY` (`id_coach`);
-
---
--- Index pour la table `messagerie1`
---
-ALTER TABLE `messagerie1`
-  ADD PRIMARY KEY (`id_message`),
-  ADD KEY `id_sportif` (`id_sportif`),
-  ADD KEY `id_coach` (`id_coach`);
-
---
--- Index pour la table `messagerie2`
---
-ALTER TABLE `messagerie2`
-  ADD PRIMARY KEY (`id_message`),
-  ADD KEY `id_sportif` (`id_sportif`),
-  ADD KEY `id_coach` (`id_coach`);
-
---
--- Index pour la table `reservation`
---
-ALTER TABLE `reservation`
-  ADD PRIMARY KEY (`id_reservation`),
-  ADD KEY `id_sport` (`id_sport`),
-  ADD KEY `id_sportif` (`id_sportif`);
-
---
--- Index pour la table `sport`
---
-ALTER TABLE `sport`
-  ADD PRIMARY KEY (`id_sport`),
-  ADD KEY `id_lieu` (`id_lieu`),
-  ADD KEY `id_sportif` (`id_sportif`);
-
---
--- Index pour la table `sportif`
---
-ALTER TABLE `sportif`
-  ADD PRIMARY KEY (`id_sportif`);
+  ADD PRIMARY KEY (`id_lieu`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
-
---
--- AUTO_INCREMENT pour la table `administrateur`
---
-ALTER TABLE `administrateur`
-  MODIFY `id_administrateur` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `coach`
@@ -290,98 +105,10 @@ ALTER TABLE `disponibilite`
   MODIFY `id_disponibilite` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pour la table `faq_coach`
---
-ALTER TABLE `faq_coach`
-  MODIFY `id_question` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `faq_sportif`
---
-ALTER TABLE `faq_sportif`
-  MODIFY `id_question` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT pour la table `lieu`
 --
 ALTER TABLE `lieu`
   MODIFY `id_lieu` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `messagerie1`
---
-ALTER TABLE `messagerie1`
-  MODIFY `id_message` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `messagerie2`
---
-ALTER TABLE `messagerie2`
-  MODIFY `id_message` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `reservation`
---
-ALTER TABLE `reservation`
-  MODIFY `id_reservation` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `sport`
---
-ALTER TABLE `sport`
-  MODIFY `id_sport` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `sportif`
---
-ALTER TABLE `sportif`
-  MODIFY `id_sportif` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `disponibilite`
---
-ALTER TABLE `disponibilite`
-  ADD CONSTRAINT `disponibilite_ibfk_1` FOREIGN KEY (`id_coach`) REFERENCES `coach` (`id_coach`) ON DELETE CASCADE,
-  ADD CONSTRAINT `disponibilite_ibfk_2` FOREIGN KEY (`id_lieu`) REFERENCES `lieu` (`id_lieu`) ON DELETE CASCADE,
-  ADD CONSTRAINT `disponibilite_ibfk_3` FOREIGN KEY (`id_reservation`) REFERENCES `reservation` (`id_reservation`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `lieu`
---
-ALTER TABLE `lieu`
-  ADD CONSTRAINT `FOREIGN KEY` FOREIGN KEY (`id_coach`) REFERENCES `coach` (`id_coach`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `messagerie1`
---
-ALTER TABLE `messagerie1`
-  ADD CONSTRAINT `messagerie1_ibfk_1` FOREIGN KEY (`id_sportif`) REFERENCES `sportif` (`id_sportif`) ON DELETE CASCADE,
-  ADD CONSTRAINT `messagerie1_ibfk_2` FOREIGN KEY (`id_coach`) REFERENCES `coach` (`id_coach`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `messagerie2`
---
-ALTER TABLE `messagerie2`
-  ADD CONSTRAINT `messagerie2_ibfk_1` FOREIGN KEY (`id_sportif`) REFERENCES `sportif` (`id_sportif`) ON DELETE CASCADE,
-  ADD CONSTRAINT `messagerie2_ibfk_2` FOREIGN KEY (`id_coach`) REFERENCES `coach` (`id_coach`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `reservation`
---
-ALTER TABLE `reservation`
-  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`id_sport`) REFERENCES `sport` (`id_sport`) ON DELETE CASCADE,
-  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`id_sportif`) REFERENCES `sportif` (`id_sportif`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `sport`
---
-ALTER TABLE `sport`
-  ADD CONSTRAINT `sport_ibfk_1` FOREIGN KEY (`id_lieu`) REFERENCES `lieu` (`id_lieu`) ON DELETE CASCADE,
-  ADD CONSTRAINT `sport_ibfk_2` FOREIGN KEY (`id_sportif`) REFERENCES `sportif` (`id_sportif`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ inscriptioncoach.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> INSCRIPTION </title>
+    <link rel="stylesheet" href="styleinscription.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+
+</head>
+
+<?php
+    if (!empty($erreurs)) {
+        echo "<ul>";
+        foreach ($erreurs as $erreur) {
+            echo "<li>" . $erreur . "</li>";
+        }
+        echo "</ul>";
+    }
+?>
+
+<body>
+    <div class="header-container">
+        <h1 align="left"> COACHUS </h1>
+        <div align="right" class="button-container">
+            <button> <a href="FAQ.html"> ? </a> </button>
+            <button> <a href="connexionsportif.html"> JE VEUX UN COACH </a> </button>
+            <button> <a href="connexioncoach.html"> JE SUIS COACH </a> </button>
+            <button> <a href="accueil.html"> ACCUEIL </a> </button>
+        </div>
+    </div>
+    <p> </p>
+    <div class='containerinscription'>
+        <div class="encadrer">
+        <h1> BOOSTEZ VOTRE CARRIÈRE DE COACH ! </h1>
+        <p>Vous êtes un coach dynamique, prêt à partager votre expertise et à aider des personnes à atteindre leurs objectifs ? <strong>Coachus</strong> vous offre une plateforme simple et dédiée pour faire évoluer votre métier.</p>
+        <p>En vous inscrivant, vous bénéficierez de nombreux avantages :</p>
+        <ul>
+            <li><strong>Développez votre réseau</strong> : Connectez-vous avec des clients motivés et élargissez votre cercle professionnel.</li>
+            <li><strong>Gérez facilement vos créneaux</strong> : Planifiez vos séances, suivez vos progrès et organisez vos rendez-vous en toute simplicité.</li>
+            <li><strong>Gagnez en visibilité</strong> : Mettez en avant vos compétences et spécialités pour attirer de nouveaux clients et diversifier votre activité.</li>
+        </ul>
+
+        <p>Rejoignez-nous dès aujourd'hui et faites grandir votre activité de coach !</p>
+
+        </div>
+        <form method="POST" action="requete_inscriptioncoach.php">
+            <div class="encadrer">
+                <h1 align="center"> INSCRIPTION </h1>
+
+                <div class="search-container">
+                    <input type="text" name="nom" placeholder="NOM" required/>
+                </div>
+            
+                <div class="search-container">
+                    <input type="text" name="prenom" placeholder="PRENOM"/>
+                </div>
+                    
+                <div class="search-container">
+                    <input type="text" name="adresse" placeholder="ADRESSE"/>
+                </div>
+                
+                <div class="search-container">
+                    <input type="text" name="numero_de_telephone" placeholder="NUMERO DE TELEPHONE"/>
+                </div>
+        
+                <div class="search-container">
+                    <input type="text" name="adresse_mail" placeholder="ADRESSE MAIL"/>
+                </div>
+                
+                <div class="search-container">
+                    <input type="text" name="confirme_adresse_mail" placeholder="CONFIRMER L'ADRESSE MAIL" required/>
+                </div>
+
+                <div class="search-container">
+                    <input type="text" name="identifiant" placeholder="IDENTIFIANT"/>
+                </div>
+
+                <div class="search-container">
+                    <input type="password" name="mot_de_passe" placeholder="MOT DE PASSE"/>
+                </div>
+
+                <div class="search-container">
+                    <input type="password" name="confirme_mot_de_passe" placeholder="CONFIRMER LE MOT DE PASSE" required/>
+
+                <button type="submit"> S'INSCRIRE </button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
