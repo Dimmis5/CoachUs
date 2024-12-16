bf1322ed8b3dfd6ab7a4fc0ebf513d2e925bb8e8
Oriane5 <orco63803@eleve.isep.fr>
2024-12-16 14:45:14 +0100

    16/12 (3)

--- connexion.php
+++ connexion.php
@@ -1,15 +1,21 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 <?php	
 $db = new PDO("mysql:host=localhost;dbname=coachus", "root", "");
 ?>
 =======
+=======
+>>>>>>> Stashed changes
 <?php
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "coachus";
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
@@ -17,4 +23,7 @@ if ($conn->connect_error) {
 }
 
 ?>
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
